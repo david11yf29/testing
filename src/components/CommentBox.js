95de5d91c3b37ef465edc4cbfe855144bcd67c89
david11yf29 <david11yf29@gmail.com>
2020-10-12 20:47:12 +0800

    updated

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-export default () => {
-  return (
-    <div>
-      Comment Box
-    </div>
-  )
+class CommentBox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      comment: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      comment: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form>
+        <h4>Add a Comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    )
+  }
 }
+
+export default CommentBox;
