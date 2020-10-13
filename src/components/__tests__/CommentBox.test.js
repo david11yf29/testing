de0ad6af79a6c58053226c2cf1d18b78dd6d736d
david11yf29 <david11yf29@gmail.com>
2020-10-13 18:53:11 +0800

    updated

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
-
+import { mount } from 'enzyme';
 import CommetBox from 'components/CommentBox';
 
 it('has a text area and a button',() => {
-  
+  const wrapped = mount(<CommetBox />);
+
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
 })
