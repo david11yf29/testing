d8591a4ed02868bbcd77df0f7ad48bd7361ab67e
david11yf29 <david11yf29@gmail.com>
2020-10-13 20:40:37 +0800

    updated

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -20,5 +20,20 @@ it('has a text area and a button',() => {
 it('has a text area that users can type in',() => {
   wrapped.find('textarea').simulate('change', {
     target: { value: 'new comment' }
-  })
+  });
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+});
+
+it('when form is submitted, text area gets emptied', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
 })
