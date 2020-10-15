226e472b9c4bff21d04b9ec9d818e4eccb16b283
david11yf29 <david11yf29@gmail.com>
2020-10-15 16:37:46 +0800

    updated

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
+beforeEach(() => { 
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
+  })
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', (done) => {
   const wrapped = mount(
     <Root>
       <App />
@@ -12,5 +25,11 @@ it('can fetch a list of comments and display them', () => {
 
   wrapped.find('.fetch-comments').simulate('click');
 
-  expect(wrapped.find('li').length).toEqual(500);
+  setTimeout(() => {
+    wrapped.update();
+    expect(wrapped.find('li').length).toEqual(2);
+
+    done(); 
+    wrapped.unmount();
+  }, 100)
 })
