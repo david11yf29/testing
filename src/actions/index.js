e049fe27dcfce067db59c5459e2be3fa41d7f461
david11yf29 <david11yf29@gmail.com>
2020-10-15 08:12:29 +0800

    updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,13 @@
 import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import axios from 'axios';
 
 export function saveComment(comment) {
   return {
     type: SAVE_COMMENT,
     payload: comment
   }
+};
+
+export function fetchComments() {
+
 }
